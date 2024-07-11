c6f4d683ed596bfad93e01699bba0285c646cd78
Danny Hobby <danny.hobby@outlook.com>
2024-07-11 15:37:14 +0100

    Rate limiting and tickets

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ticket;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -11,9 +12,13 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run() : void
     {
-        // User::factory(10)->create();
+        $users = User::factory(10)->create();
+
+        Ticket::factory(100)
+            ->recycle($users)
+            ->create();
 
         User::factory()->create([
             'name' => 'Test User',
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,22 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// domain/api/resource/{identifier}/action
+// Resources - Models
+// tickets
+// users
+// contracts
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/tickets', function () {
+    return Ticket::all();
+})->middleware(['throttle:tickets']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
