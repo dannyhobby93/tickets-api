28fdd2c289b705688ed15ba5263ca6893270b63c
Danny Hobby <danny.hobby@outlook.com>
2024-07-26 12:02:00 +0100

    Refactor for lrvl 11

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -5,21 +5,15 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponses;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
     use ApiResponses;
 
-    // protected string $namespace = 'App\\Policies\\V1';
     protected string $policy_class;
 
     public function __construct()
     {
-        // Gate::guessPolicyNamesUsing(
-        //     fn(string $modelClass) =>
-        //     "{$this->namespace}\\" . class_basename($modelClass) . "Policy"
-        // );
         Gate::guessPolicyNamesUsing(function () {
             return $this->policy_class;
         });
@@ -37,9 +31,4 @@ class ApiController extends Controller
 
         return in_array(strtolower($relationship), $include_values);
     }
-
-    // public function isAble($ability, $target_model)
-    // {
-    //     return $this->authorize($ability, [$target_model, $this->policy_class]);
-    // }
 }
